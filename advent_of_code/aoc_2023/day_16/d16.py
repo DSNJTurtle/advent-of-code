2388fb69189ba6185b191538cbf86321d6055546
DSNJTurtle <sebastian.t.kirchner@gmail.com>
2023-12-16 23:36:02 +0100

    fix: improve day 16

--- advent_of_code/aoc_2023/day_16/d16.py
+++ advent_of_code/aoc_2023/day_16/d16.py
@@ -1,132 +1,93 @@
+"""Replace caching solution with complex numbers. Inspiration taken from https://topaz.github.io/paste/#XQAAAQAUAwAAAAAAAAAziAOiE/kI+atcxglPNa1Z5ByKIiDHt58MHifgG2R3rf+zqKYQZv1B457oZ4kCsEABs+9eoKiNQRgfhGTI51bj3uRshO3PWkcO2ujzAaCpLjowdp1GKNTOnkukUGRQKdOcsHDROmva3wF9HKvBRmDVX57YyOReLWiXWu7VZ6BH+4I4+HYwrpO4f3Cnm0z1SibCaOiKz4eojGMOAGzZZpSzQOL3fz30dX5Pyz12ucW45Yuu+ZiaJ7UdH/MYB9mWGHziy8uujcM+VxQUCWPjkRQ0XdsUM3PC+CTKPYXP2Gyaq+wyME55uuGsNGBw46QrFtVC+Shv2NuT8LF2oxefM5bcWLHsbpmwLCIV0EqhAiT5KgU7RRDjVOGA4hLybRH7NlZSaFrNUASfRa+QEb2StcGlaaQnm5IpLIJ0ngBwFVo1XbqqDcxOKAMUfyPEs1EQR2JOm/9/+sQBjXgiCNWpr//6SPk0"""
 from __future__ import annotations
 
-from functools import cache
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
 from tqdm import tqdm
 
 from advent_of_code.commons.commons import read_input_to_list
 
-MOVING_DIRECTION_SPLITS = {
-    "R": {"/": ["U"], "\\": ["D"], "|": ["U", "D"]},
-    "L": {"/": ["D"], "\\": ["U"], "|": ["U", "D"]},
-    "U": {"/": ["R"], "\\": ["L"], "-": ["L", "R"]},
-    "D": {"/": ["L"], "\\": ["R"], "-": ["L", "R"]},
-}
-
-
-def inner_single_move(
-    grid: np.ndarray, start_cell: Tuple[int, int], direction: str
-) -> Tuple[Tuple[int, int], List[Tuple[int, int]]]:
-    m, n = start_cell
-    energized_cells = [start_cell]
-    if direction == "R":
-        jj = n + 1
-        for jj in range(n + 1, len(grid[m])):
-            if grid[m, jj] in MOVING_DIRECTION_SPLITS[direction]:
-                break
-            energized_cells.append((m, jj))
-
-        return (m, jj), energized_cells
-
-    elif direction == "L":
-        jj = n - 1
-        for jj in range(n - 1, -1, -1):
-            if grid[m, jj] in MOVING_DIRECTION_SPLITS[direction]:
-                break
-            energized_cells.append((m, jj))
-
-        return (m, jj), energized_cells
-
-    elif direction == "U":
-        ii = m - 1
-        for ii in range(m - 1, -1, -1):
-            if grid[ii, n] in MOVING_DIRECTION_SPLITS[direction]:
-                break
-            energized_cells.append((ii, n))
-
-        return (ii, n), energized_cells
-
-    elif direction == "D":
-        ii = m + 1
-        for ii in range(m + 1, len(grid[:, n])):
-            if grid[ii, n] in MOVING_DIRECTION_SPLITS[direction]:
-                break
-            energized_cells.append((ii, n))
-
-        return (ii, n), energized_cells
-
-    else:
-        raise RuntimeError("invalid direction")
-
 
 def parse_grid(lines: List[str]):
-    grid = []
-    for l in lines:
-        grid.append([str(s) for s in l])
-
-    return np.array(grid)
+    return {complex(i, j): c for i, r in enumerate(lines) for j, c in enumerate(r)}
 
 
-def energised_tiles_from_starting_position(grid, starting_cell, starting_move, move_func):
-    n_rows = len(grid)
-    n_cols = len(grid[0])
+def energised_tiles_from_starting_position(grid, start_pos, start_d):
+    # pgrid = PrintGrid(grid)
+    # right/left = 1j/-1j, up/down = -1/+1
+    todo = [(start_pos, start_d)]
+    done = set()
+    while todo:
+        pos, d = todo.pop()
+        while not (pos, d) in done:
+            done.add((pos, d))
+            pos += d
+            # pgrid.update([pos])
+            match grid.get(pos):
+                case "|":
+                    d = -1
+                    todo.append((pos, -d))
+                case "-":
+                    d = 1j
+                    todo.append((pos, -d))
+                case "/":
+                    d = (d * 1j).conjugate()  # -complex(im,re)
+                case "\\":
+                    d = -(d * 1j).conjugate()  # complex(im,re)
+                case None:
+                    break  # skip "."
 
-    next_moves = [(starting_cell, starting_move)]
-    already_visited = set(next_moves)
-    energised_cells = list()
+            # pgrid.show()
 
-    while next_moves:
-        s_cell, moving_direction = next_moves.pop()
-        already_visited.add((s_cell, moving_direction))
-        next_cell, ec = move_func(s_cell, moving_direction)
-        i, j = next_cell
-        if 0 <= i < n_rows and 0 <= j < n_cols:
-            # otherwise reached end
-            for next_moving_dir in MOVING_DIRECTION_SPLITS[moving_direction].get(
-                grid[i, j], moving_direction
-            ):
-                nm = (next_cell, next_moving_dir)
-                if nm not in already_visited:
-                    next_moves.append(nm)
+    pos = list({pos for pos, _ in done})
 
-        energised_cells.extend(ec)
-        energised_cells = list(set(energised_cells))
+    return len(pos) - 1  # remove start
 
-    return len(energised_cells) - 1  # remove  start cell
+
+class PrintGrid:
+    def __init__(self, grid, initial="."):
+        self.initial = initial
+        n_rows = int(max(k.real for k in grid)) + 1
+        n_cols = int(max(k.imag for k in grid)) + 1
+        self.grid = np.zeros((n_rows, n_cols), dtype=int)
+
+    def show(self):
+        print()
+        for g in self.grid:
+            print(" ".join([str(i) for i in g]).replace("0", self.initial))
+
+        print()
+        print()
+
+    def update(self, entries: List[complex]) -> None:
+        for e in entries:
+            re, im = e.real, e.imag
+            if 0 <= re < len(self.grid) and 0 <= im < len(self.grid[0]):
+                self.grid[int(e.real)][int(e.imag)] += 1
 
 
 def part_a(lines: List[str]) -> int:
     grid = parse_grid(lines)
-
-    @cache
-    def move(start_cell: Tuple[int, int], direction: str) -> Tuple[Tuple[int, int], List[Tuple[int, int]]]:
-        return inner_single_move(grid, start_cell, direction)
-
-    return energised_tiles_from_starting_position(grid, (0, -1), "R", move)
+    return energised_tiles_from_starting_position(grid, -1j, 1j)
 
 
 def part_b(lines: List[str]) -> int:
     grid = parse_grid(lines)
 
-    @cache
-    def move(start_cell: Tuple[int, int], direction: str) -> Tuple[Tuple[int, int], List[Tuple[int, int]]]:
-        return inner_single_move(grid, start_cell, direction)
-
     starting_positions = []
     # from left and right
-    for i in range(len(grid)):
-        starting_positions.append(((i, -1), "R"))
-        starting_positions.append(((i, len(grid[0])), "L"))
+    for i in range(len(lines)):
+        starting_positions.append((complex(i, -1), 1j))
+        starting_positions.append((complex(i, len(lines[0])), -1j))
     # from top and bottom
-    for j in range(len(grid[0])):
-        starting_positions.append(((-1, j), "D"))
-        starting_positions.append(((len(grid), j), "U"))
+    for j in range(len(lines[0])):
+        starting_positions.append((complex(-1, j), 1))
+        starting_positions.append((complex(len(lines), j), -1))
 
     tiles = []
     for sc, md in tqdm(starting_positions):
-        tiles.append(energised_tiles_from_starting_position(grid, sc, md, move))
+        tiles.append(energised_tiles_from_starting_position(grid, sc, md))
 
     return np.max(tiles)
 
